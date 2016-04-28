9c15adb13d5ed1bde0d41aa52dc45e5335cf25bc
Shreyas Jaganmohan <shreyasjaganmohan@Shreyass-MacBook-Air.local>
2016-04-28 17:30:51 -0400

    feat(verification/backends/email): add email backend class, unit tests for PhoneVerificationService and test_backend

---
+++ tests/test_services.py
@@ -0,0 +1,64 @@
+# !/usr/bin/env python
+#  -*- coding: utf-8 -*-
+
+"""
+test_django-user-verification
+------------
+
+Tests for `services` models module.
+"""
+
+# Third Party Stuff
+from django.test import TestCase
+from nose.tools import ok_
+from rest_framework.test import APIRequestFactory
+from mock import patch
+from django.core.urlresolvers import reverse 
+from time import sleep
+
+# Local Stuff
+from verification.services import PhoneVerificationService
+
+
+class TestVerification(TestCase):
+
+    def setUp(self):
+        self.service = PhoneVerificationService()
+        factory = APIRequestFactory()
+        self.request = factory.get('/')
+
+        self.phone_number = '+13478379634'
+
+    @patch('verification.backends.twilio.TwilioBackend.send')
+    def test_send_verification_generates_message_and_sends_message(self, mock):
+        self.service.send_verification(self.phone_number, self.request)
+
+        ok_(mock.called)
+
+        args, kwargs = mock.call_args
+
+        # Check if url is in
+        ok_('verify' in args[1])
+
+    def test_create_url_generates_absolute_uri_for_verify_endpoint(self):
+        uri = self.service.create_url(self.request, 'key')
+        self.assertEquals(uri, 'http://testserver/verify/key/')
+
+    @patch('verification.services.PhoneVerificationService.generator')
+    def test_create_temporary_token_with_default_expiry(self, mock):
+        mock.return_value = 55555
+        token = self.service.create_temporary_token(number=self.phone_number)
+        self.assertEquals(token, 55555)
+        self.assertEquals(self.service.check_pin(token, self.phone_number), True)
+
+
+    @patch('verification.services.PhoneVerificationService.generator')
+    def test_create_temporary_token_with_expiry_value_zero(self, mock):
+        mock.return_value = 55555
+        token = self.service.create_temporary_token(number=self.phone_number, expiry=0)
+        sleep(1)
+        self.assertEquals(self.service.check_pin(token, self.phone_number), False)
+
+
+    def tearDown(self):
+        pass
--- verification/backends/__init__.py
+++ verification/backends/__init__.py
@@ -6,11 +6,12 @@ from django.utils.module_loading import import_string
 
 DEFAULT_SERVICE = 'verification.backends.base.BaseBackend'
 
-backend = None
+phone_backend = None
+email_backend = None
 
 
-def get_sms_backend():
-    if not backend:
+def get_phone_backend():
+    if not phone_backend:
         backend_import = DEFAULT_SERVICE
 
         if settings.PHONE_VERIFICATION.get('BACKEND', None):
@@ -18,3 +19,14 @@ def get_sms_backend():
 
         backend_cls = import_string(backend_import)
         return backend_cls(**settings.PHONE_VERIFICATION['OPTIONS'])
+
+
+def get_email_backend():
+    if not email_backend:
+        backend_import = DEFAULT_SERVICE
+
+        if settings.EMAIL_VERIFICATION.get('BACKEND', None):
+            backend_import = settings.EMAIL_VERIFICATION['BACKEND']
+
+        backend_cls = import_string(backend_import)
+        return backend_cls(**settings.EMAIL_VERIFICATION['OPTIONS'])
