da21fa62acdad4024c46764a5d4a73310d068087
Shreyas Jaganmohan <shreyasjaganmohan@Shreyass-MacBook-Air.local>
2016-04-28 17:30:51 -0400

    feat(tests/test_models): add unit tests for PhoneVerificationService

--- verification/backends/base.py
+++ verification/backends/base.py
@@ -7,8 +7,5 @@ class BaseBackend(object):
     def __init__(self, **settings):
         pass
 
-    def send_sms(self, numbers, message):
-        pass
-
-    def send_bulk_sms(self, numbers, message):
+    def send(self, numbers, message):
         pass
